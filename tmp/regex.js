594691a1d30b3f6c67eff61f6cfce2d9b5a75cb3
KostasEreksonas <k.ereksonas@gmail.com>
2023-01-07 19:57:27 +0200

    Removed irrelevant files

--- tmp/regex.js
+++
@@ -1,6 +0,0 @@
-// Regex
-// ---------------------------------------------------------------------------------------------------------
-let string = '[ 1 2 a 3 p 5 asd 6 xcvbn \'';
-let regex = /[0-9]/g;
-let skaiciai = string.match(regex);
-console.log(skaiciai);
--- tmp/tabai.js
+++
@@ -1,8 +0,0 @@
-let sheets = document.getElementsByClassName('sheet');
-
-function changeTab(tabId) {
-    for (let i = 0; sheets.length > i; i++) {
-        sheets[i].style.display = 'none';
-    }
-    document.getElementById(tabId).style.display = 'block';
-}
